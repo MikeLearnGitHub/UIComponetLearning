1b23f294866867370a326d0b092ddae0eee78d96
mike2004_chen@hotmail.com <mike2004_chen@hotmail.com>
2021-03-08 00:04:32 -0700

    change activity to use view binding

--- app/src/main/java/com/example/uicomponetexample/MainActivity.kt
+++ app/src/main/java/com/example/uicomponetexample/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.uicomponetexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.uicomponetexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        initView()
+    }
+
+    private fun initView() {
+        binding.btnUIComponent.setOnClickListener {
+            Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show()
+        }
     }
 }
